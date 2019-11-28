d8643ec11f3f0a09e4f0b048f44438046bfd14d9
Mario Uher <uher.mario@gmail.com>
2019-11-28 18:09:29 +0100

    Add basic WiFi

--- sketch.ino
+++ sketch.ino
@@ -1,12 +1,54 @@
+#include <ESP8266WiFi.h>
+#include "./config.h"
+
+WiFiClient wifi;
+
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(115200);
+  while (!Serial)
+  { // Wait for the serial port to connect
+    Serial.println();
+  }
+
+  setupWiFi();
+  blink(2);
+}
+
+void setupWiFi()
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  Serial.print("Connecting to ");
+  Serial.print(WIFI_SSID);
+  Serial.print(" ");
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(250);
+    Serial.print(".");
+  }
+
+  Serial.println(" done!");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void blink(int count)
+{
+  for (int i = 0; i < count; i++)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(150);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(150);
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
 }
 
 void loop()
 {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
 }
